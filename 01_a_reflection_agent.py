20809f2afdf3fa9d77d0ad8dfc102df27092e5b8
rajib76 <rajib76@yahoo.com>
2024-06-14 22:09:20 -0700

    reflection

--- 01_a_reflection_agent.py
+++ 01_a_reflection_agent.py
@@ -5,7 +5,7 @@ import os
 from typing import TypedDict, Annotated, Sequence, List
 
 from dotenv import load_dotenv
-from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.messages import BaseMessage, HumanMessage, AnyMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import OpenAI, ChatOpenAI
 from langgraph.constants import END
@@ -21,8 +21,8 @@ llm = ChatOpenAI()
 
 # The shared state of the agents
 class State(TypedDict):
-    # messages: Annotated[Sequence[BaseMessage], operator.add]
     messages: Annotated[Sequence[BaseMessage], operator.add]
+
 
 
 class ReflectionAgent(BaseModel):
@@ -30,6 +30,7 @@ class ReflectionAgent(BaseModel):
 
     # The first agent in the workflow that generates the answer
     def generate_answer(self, state: State):
+        messages = state["messages"]
         generation_prompt = ChatPromptTemplate.from_messages([
             (
                 "system",
@@ -40,7 +41,8 @@ class ReflectionAgent(BaseModel):
             ),
             MessagesPlaceholder(variable_name="messages"), ])
         generate = generation_prompt | llm
-        return generate.invoke({"agent":"generate","messages": state})
+        messages = generate.invoke({"messages": messages})
+        return {"messages":[messages.content]}
 
     # The first agent in the workflow that critiques the answer
     def critique_answer(self, state: State):
@@ -56,18 +58,21 @@ class ReflectionAgent(BaseModel):
             ]
         )
         reflect = reflection_prompt | llm
-        original_question = [HumanMessage(content=state[0].content)]
-        answer = [HumanMessage(content=state[-1].content)]
-        return reflect.invoke({"agent":"reflect","messages": original_question + answer})
+        original_question = state["messages"][0]
+        answer = state["messages"][-1]
+        # original_question = [HumanMessage(content=state[0].content)]
+        # answer = [HumanMessage(content=state[-1].content)]
+        messages = reflect.invoke({"messages": [original_question + answer]})
+        return {"messages":[messages.content]}
 
     def should_continue(self, state: List[BaseMessage]):
-        if len(state) > 8:
+        if len(state["messages"]) > 8:
             # End after 4 iterations
             return END
         return "critique_agent"
 
     def make_workflow(self):
-        graph_builder = MessageGraph()
+        graph_builder = StateGraph(State)
         graph_builder.add_node("generate_agent", self.generate_answer)
         graph_builder.add_node("critique_agent", self.critique_answer)
         graph_builder.set_entry_point("generate_agent")
@@ -87,6 +92,7 @@ if __name__ == "__main__":
         if user_input.lower() in ["quit", "exit", "q"]:
             print("Goodbye!")
             break
-        for event in graph.stream([HumanMessage(content=user_input)]):
+        for event in graph.stream({"messages":[user_input]}):
             for key,value in event.items() :
-                print(str(key) + ":" + str(value))
+                print(str(key) + ":" + str(value["messages"][0]))
+
